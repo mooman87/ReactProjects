af1adbc81de1520ed0334360258b0774c4c524ba
Jeremy <64501700+mooman87@users.noreply.github.com>
2021-04-28 10:33:55 -0400

    Update Contact.jsx
    Update with API

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,111 +1,165 @@
+/* jshint esversion: 6 */
+
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Footer";
 import { Container } from "react-bootstrap";
 import axios from "axios";
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
 
-export default class MessageList extends React.Component {
-  state = {
-    name: "",
-    email: "",
-    message: ""
+var router = express.Router();
+
+const creds = require("../config");
+
+var transport = {
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS
+  }
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
+});
+
+router.post("/send", (req, res, next) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `;
+
+  var mail = {
+    from: name,
+    to: "pikeportrait@gmail.com",
+    subject: "New Message from Contact Form",
+    text: content
   };
 
-  handleChange = (event) => {
-    this.setState({ name: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    const user = {
-      name: this.state.name
-    };
-
-    axios
-      .post(`https://jsonplaceholder.typicode.com/users`, { user })
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail"
       });
-  };
+    } else {
+      res.json({
+        status: "success"
+      });
+    }
+  });
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(3002);
+
+class EmailMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "http://localhost:3002/send",
+      data: this.state
+    }).then((response) => {
+      if (response.data.status === "success") {
+        alert("Message Sent.");
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        alert("Message failed to send.");
+      }
+    });
+  }
+
+  resetForm() {
+    this.setState({ name: "", email: "", message: "" });
+  }
 
   render() {
     return (
       <Container fluid="sm">
-        <div style={{ paddingTop: 40 }}>
-          <form onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input
-              class="form-control"
-              type="text"
-              name="name"
-              onChange={this.handleChange}
-            />
-            <label>Email</label>
-            <input class="form-control" type="text" name="email" />
-
-            <label>Message</label>
-            <textarea
-              class="form-control"
-              name="message"
-              cols="30"
-              rows="5"
-            ></textarea>
-            <button
-              type="submit"
-              name="publish"
-              autocomplete="off"
-              class="btn btn-success"
-            >
+        <div className="EmailMessage" style={{ paddingTop: 50 }}>
+          <h1>Contact Us</h1>
+          <p>
+            You've had a chance to learn about us, now let's learn about you and
+            your vision...
+          </p>
+          <form
+            id="contact-form"
+            onSubmit={this.handleSubmit.bind(this)}
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={this.state.name}
+                onChange={this.onNameChange.bind(this)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                value={this.state.email}
+                onChange={this.onEmailChange.bind(this)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea
+                className="form-control"
+                rows="5"
+                id="message"
+                value={this.state.message}
+                onChange={this.onMessageChange.bind(this)}
+              />
+            </div>
+            <button type="submit" className="btn btn-success">
               Submit
             </button>
           </form>
-          <Footer />
         </div>
+        <Footer />
       </Container>
     );
   }
+
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  onMessageChange(event) {
+    this.setState({ message: event.target.value });
+  }
 }
 
-// function Contact() {
-//   return (
-//     <Container fluid="lg">
-//       <div>
-//         <div>
-//           <h1>Contact Us</h1>
-//           <p>
-//             You've learned about us, so tell us about you and your vision...
-//           </p>
-//           <form class="blog" action="/contact" method="post">
-//             <div class="form-group">
-//               <label>Name</label>
-//               <input class="form-control" type="text" name="msgSubject" />
-//               <label>Email</label>
-//               <input class="form-control" type="text" name="msgSubject" />
-
-//               <label>Message</label>
-//               <textarea
-//                 class="form-control"
-//                 name="msgBody"
-//                 cols="30"
-//                 rows="5"
-//               ></textarea>
-//             </div>
-//             <button
-//               type="submit"
-//               name="publish"
-//               autocomplete="off"
-//               class="btn btn-success"
-//             >
-//               Send
-//             </button>
-//           </form>
-//         </div>
-//         <Footer />
-//       </div>
-//     </Container>
-//   );
-// }
-
-// export default Contact;
+export default EmailMessage;
