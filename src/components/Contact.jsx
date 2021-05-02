3cf1d82ea655d610d74ddce6b096c7269826a08f
Jeremy <64501700+mooman87@users.noreply.github.com>
2021-05-02 10:18:10 -0400

    Update Contact.jsx
    Include axios dependency

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,165 +1,109 @@
-/* jshint esversion: 6 */
-
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Footer";
-import { Container } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import axios from "axios";
-import express from "express";
-import nodemailer from "nodemailer";
-import cors from "cors";
 
-var router = express.Router();
+const Contact = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: ""
+  });
 
-const creds = require("../config");
+  const [result, setResult] = useState(null);
 
-var transport = {
-  host: "smtp.gmail.com",
-  port: 587,
-  auth: {
-    user: creds.USER,
-    pass: creds.PASS
-  }
-};
-
-var transporter = nodemailer.createTransport(transport);
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take messages");
-  }
-});
-
-router.post("/send", (req, res, next) => {
-  var name = req.body.name;
-  var email = req.body.email;
-  var message = req.body.message;
-  var content = `name: ${name} \n email: ${email} \n message: ${message} `;
-
-  var mail = {
-    from: name,
-    to: "pikeportrait@gmail.com",
-    subject: "New Message from Contact Form",
-    text: content
+  const sendEmail = (event) => {
+    event.preventDefault();
+    axios
+      .post("/send", { ...state })
+      .then((response) => {
+        setResult(response.data);
+        setState({
+          name: "",
+          email: "",
+          subject: "",
+          message: ""
+        });
+      })
+      .catch(() => {
+        setResult({
+          success: false,
+          message: "Something went wrong. Try again later."
+        });
+      });
   };
 
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: "fail"
-      });
-    } else {
-      res.json({
-        status: "success"
-      });
-    }
-  });
-});
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(3002);
+    setState({ ...state, [name]: value });
+  };
 
-class EmailMessage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      message: ""
-    };
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    axios({
-      method: "POST",
-      url: "http://localhost:3002/send",
-      data: this.state
-    }).then((response) => {
-      if (response.data.status === "success") {
-        alert("Message Sent.");
-        this.resetForm();
-      } else if (response.data.status === "fail") {
-        alert("Message failed to send.");
-      }
-    });
-  }
-
-  resetForm() {
-    this.setState({ name: "", email: "", message: "" });
-  }
-
-  render() {
-    return (
-      <Container fluid="sm">
-        <div className="EmailMessage" style={{ paddingTop: 50 }}>
-          <h1>Contact Us</h1>
-          <p>
-            You've had a chance to learn about us, now let's learn about you and
-            your vision...
+  return (
+    <Container fluid="sm">
+      <div className="Contact" style={{ paddingTop: 50 }}>
+        <h1>Contact Us</h1>
+        <p>
+          You've had a chance to learn about us, now let's learn about you and
+          your vision...
+        </p>
+        {result && (
+          <p className={`${result.success ? "success" : "error"}`}>
+            {result.message}
           </p>
-          <form
-            id="contact-form"
-            onSubmit={this.handleSubmit.bind(this)}
-            method="POST"
-          >
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                value={this.state.name}
-                onChange={this.onNameChange.bind(this)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                aria-describedby="emailHelp"
-                value={this.state.email}
-                onChange={this.onEmailChange.bind(this)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea
-                className="form-control"
-                rows="5"
-                id="message"
-                value={this.state.message}
-                onChange={this.onMessageChange.bind(this)}
-              />
-            </div>
-            <button type="submit" className="btn btn-success">
-              Submit
-            </button>
-          </form>
-        </div>
+        )}
+        <form onSubmit={sendEmail}>
+          <Form.Group controlId="name">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              value={state.name}
+              placeholder="Enter your full name"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="text"
+              name="email"
+              value={state.email}
+              placeholder="Enter your email"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="subject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type="text"
+              name="subject"
+              value={state.subject}
+              placeholder="Enter subject"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="subject">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="message"
+              value={state.message}
+              rows="3"
+              placeholder="Enter your message"
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </form>
         <Footer />
-      </Container>
-    );
-  }
+      </div>
+    </Container>
+  );
+};
 
-  onNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  onEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
-
-  onMessageChange(event) {
-    this.setState({ message: event.target.value });
-  }
-}
-
-export default EmailMessage;
+export default Contact;
